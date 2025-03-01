730481d29ea2f08bf82b26ab2fa5128ff8802223
Rohid <rohid10@live.be>
2025-03-01 15:05:00 +0100

    Project setup

---
+++ IncidentsAppApi/Controllers/IncidentController.cs
@@ -0,0 +1,36 @@
+﻿using IncidentsAppApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IncidentsAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentController : ControllerBase
+    {
+        static private List<Incident> incidents = new List<Incident>
+        {
+            new Incident
+            {
+                Id = 1,
+                Description = "Poep op de stoep",
+                Status = "Gemeld",
+                Priority = "Hoog"
+            },
+            
+            new Incident
+            {
+                Id = 2,
+                Description = "Omgevallen boom",
+                Status = "Geregistreerd",
+                Priority = "Hoog"
+            },
+        };
+
+        [HttpGet]
+        public ActionResult<List<Incident>> GetIncidents()
+        {
+            return Ok(incidents);
+        }
+    }
+}
---
+++ IncidentsAppApi/Models/Incident.cs
@@ -0,0 +1,10 @@
+﻿namespace IncidentsAppApi.Models
+{
+    public class Incident
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+    }
+}
