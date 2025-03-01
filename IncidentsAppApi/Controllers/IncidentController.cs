90273b7a0a6d01767b4f5d98e7d82a3681ea6b44
Rohid <rohid10@live.be>
2025-03-01 16:03:15 +0100

    Added GetById, Post and Update

--- IncidentsAppApi/Controllers/IncidentController.cs
+++ IncidentsAppApi/Controllers/IncidentController.cs
@@ -14,23 +14,76 @@ namespace IncidentsAppApi.Controllers
             {
                 Id = 1,
                 Description = "Poep op de stoep",
-                Status = "Gemeld",
+                Status = "In behandeling",
                 Priority = "Hoog"
             },
-            
+
             new Incident
             {
                 Id = 2,
                 Description = "Omgevallen boom",
-                Status = "Geregistreerd",
+                Status = "In behandeling",
                 Priority = "Hoog"
+            },
+
+            new Incident
+            {
+                Id = 3,
+                Description = "Graffiti op de muur in een tunnel",
+                Status = "Gemeld",
+                Priority = "Laag"
             },
         };
 
         [HttpGet]
-        public ActionResult<List<Incident>> GetIncidents()
+        public ActionResult<List<Incident>> GetAllIncidents()
         {
             return Ok(incidents);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Incident> GetIncidentById(int id)
+        {
+            var incident = incidents.FirstOrDefault(i => i.Id == id);
+            if (incident is null)
+            {
+                return NotFound();
+            }
+            return Ok(incident);
+        }
+
+        [HttpPost]
+        public ActionResult<Incident> AddIncident(Incident newIncident)
+        {
+            if (newIncident is null)
+            {
+                return BadRequest();
+            }
+            newIncident.Id = incidents.Max(i => i.Id) + 1;
+            incidents.Add(newIncident);
+            return CreatedAtAction(nameof(GetIncidentById), new { id = newIncident.Id }, newIncident);
+        }
+
+        [HttpPut("{id}")]//Updates whole object
+        public IActionResult UpdateIncident(int id, Incident updatedIncident)
+        {
+            var incident = incidents.FirstOrDefault(i => i.Id == id);
+            if (incident is null)
+            {
+                return NotFound();
+            }
+
+            incident.Description = updatedIncident.Description;
+            incident.Status = updatedIncident.Status;
+            incident.Priority = updatedIncident.Priority;
+
+            return NoContent();
+        }
+
+        //[HttpDelete]
+        //public ActionResult<Incident> RemoveIncidentByid()
+        //{
+            
+        //}
     }
 }
