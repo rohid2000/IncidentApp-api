80bef0d5cb7ce2bc19dc51c94d64e2ba65645465
Rohid <rsewpersd@syntess.nl>
2025-05-08 15:01:10 +0200

    required Description property and nullable Prio

--- IncidentsAppApi/Models/Incident.cs
+++ IncidentsAppApi/Models/Incident.cs
@@ -3,9 +3,9 @@
     public class Incident
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+        public required string Description { get; set; }
         public string Status { get; set; } = "Gemeld";
-        public string Priority { get; set; }
+        public string? Priority { get; set; }
         public int UserId { get; set; }
     }
 }
