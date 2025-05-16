d803c669387366df8f56dcc45a8d33a94336c59a
Rohid <rsewpersd@syntess.nl>
2025-05-16 16:17:57 +0200

    Set value of Priority property to null

--- IncidentsAppApi/Models/Incident.cs
+++ IncidentsAppApi/Models/Incident.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public required string Description { get; set; }
         public string Status { get; set; } = "Gemeld";
-        public string? Priority { get; set; }
+        public string? Priority { get; set; } = null;
         public int UserId { get; set; }
     }
 }
