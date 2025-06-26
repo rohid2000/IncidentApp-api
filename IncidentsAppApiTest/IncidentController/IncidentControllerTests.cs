ce998f81d7a331a4377d3bb83dea86df7bfaed13
Rohid <rohid10@live.be>
2025-06-26 19:22:51 +0200

    Setup xUnit test project

---
+++ IncidentsAppApiTest/IncidentController/IncidentControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using IncidentsAppApi.Controllers;
+using IncidentsAppApi.Database;
+using IncidentsAppApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentsAppApiTest.IncidentController
+{
+    public class IncidentControllerTests
+    {
+        private readonly Mock<MyDbContext> _context;
+        private readonly IncidentsAppApi.Controllers.IncidentController _controller;
+
+        [Fact]
+        public async Task GetAllIncidents_No_Users_Found_Test()
+        {
+            //Arrange
+            
+
+            //Act
+
+
+            //Assert
+        }
+    }
+}
