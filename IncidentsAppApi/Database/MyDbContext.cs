dfd47f8e11b597eb952f06b414fa4239432706a2
Rohid <rsewpersd@syntess.nl>
2025-07-02 14:20:36 +0200

    Added 2/3 working Unit tests for GetAllIncidents

--- IncidentsAppApi/Database/MyDbContext.cs
+++ IncidentsAppApi/Database/MyDbContext.cs
@@ -5,8 +5,8 @@ namespace IncidentsAppApi.Database
 {
     public class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options)
     {
-        public DbSet<Incident> Incidents => Set<Incident>();
-        public DbSet<User> Users => Set<User>();
+        public virtual DbSet<Incident> Incidents => Set<Incident>();
+        public virtual DbSet<User> Users => Set<User>();
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
--- IncidentsAppApiTest/IncidentController/IncidentControllerTests.cs
+++ IncidentsAppApiTest/IncidentController/IncidentControllerTests.cs
@@ -1,31 +1,82 @@
-﻿using IncidentsAppApi.Controllers;
-using IncidentsAppApi.Database;
-using IncidentsAppApi.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.EntityFrameworkCore;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IncidentsAppApiTest.IncidentController
 {
+    using IncidentsAppApi.Controllers;
+    using IncidentsAppApi.Database;
+    using IncidentsAppApi.Models;
+    using Microsoft.AspNetCore.Mvc;
+
     public class IncidentControllerTests
     {
         private readonly Mock<MyDbContext> _context;
-        private readonly IncidentsAppApi.Controllers.IncidentController _controller;
+        private readonly IncidentController _controller;
+        private readonly Mock<DbSet<Incident>> _incidentSet;
+        private readonly Mock<DbSet<User>> _userSet;
+
+        public IncidentControllerTests()
+        {
+            var options = new DbContextOptions<MyDbContext>();
+            _context = new Mock<MyDbContext>(options);
+            _incidentSet = new Mock<DbSet<Incident>>();
+            _userSet = new Mock<DbSet<User>>();
+
+            // Setup the Users DbSet
+            var users = new List<User>
+            {
+                new User { Id = 1, Username = "Test", IsAdmin = false }
+            }.AsQueryable();
+
+            _userSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(users.Provider);
+            _userSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(users.Expression);
+            _userSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(users.ElementType);
+            _userSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(users.GetEnumerator());
+
+            // Mock the expression-bodied property
+            _context.SetupGet(x => x.Users).Returns(_userSet.Object);
+            _context.SetupGet(x => x.Incidents).Returns(_incidentSet.Object);
+
+            _controller = new IncidentController(_context.Object);
+        }
 
         [Fact]
-        public async Task GetAllIncidents_No_Users_Found_Test()
+        public async Task GetAllIncidents_No_Users_Found_ReturnsBadRequest()
         {
             //Arrange
-            
+            var userData = new AllIIncident { username = "" };
 
             //Act
-
+            var result = await _controller.GetAllIncidents(userData);
 
             //Assert
+            Assert.IsType<BadRequestResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task GetAllIncidents_No_Users_Found_As_Admin_ReturnsBadRequest()
+        {
+            // Arrange
+            var userData = new AllIIncident { username = "Test" };
+
+            // Act
+            var result = await _controller.GetAllIncidents(userData);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task GetAllIncidents_With_ResultOk()
+        {
+            //Arrange
+            var userData = new AllIIncident { username = "Test" };
+
+            //Act
+            var result = await _controller.GetAllIncidents(userData);
+
+            //Assert
+            Assert.IsType<OkResult>(result.Result);
         }
     }
 }
